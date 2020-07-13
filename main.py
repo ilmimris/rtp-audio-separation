5ba86acd7a5ef6eb627673b39cdf3beb75860e83
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2020-07-13 07:32:20 +0700

    WIP: use config file yaml

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
-# import yaml
-# import argparse
+import yaml
+import argparse
 import pyshark
 import pywav
 
+DEBUG = False
+
 # construct the argument parser and parse the arguments
-# ap = argparse.ArgumentParser(description='RTP Audio Parser')
-# ap.add_argument("-i", "--input", required=True, help="input PCAP file or device (eno1)")
-# ap.add_argument("-o", "--output", required=True, help="path/to/output file")
-# ap.add_argument("-c", "--config", required=True, help="parser configuration path/to/config.yml")
+ap = argparse.ArgumentParser(description='RTP Audio Parser')
+ap.add_argument("-i", "--input", required=True, help="input PCAP file or network interface (eno1)")
+ap.add_argument("-o", "--outdir", required=True, help="path/to/output directory")
+ap.add_argument("-c", "--config", required=False, help="parser configuration path/to/config.yml")
 
-# args = vars(ap.parse_args())
+args = vars(ap.parse_args())
 
-rtp_list = {}
-rtp_codec_list = {}
+# rtp_list = {}
+# rtp_codec_list = {}
 
 def collectingPayloadBySession(rtp: pyshark.packet.layer.Layer
     , container: list) -> list: 
@@ -64,34 +66,51 @@ def raw2wav(audio: bytearray, fn: str
 
 def openPCAP(pcap_file: str, display_filter) -> pyshark.capture.file_capture.FileCapture:
     print(f"Scraping: {pcap_file} with filter '{display_filter}'")
-    return pyshark.FileCapture(pcap_file, display_filter=display_filter)
+    cap = pyshark.FileCapture(pcap_file, display_filter=display_filter, debug=DEBUG)
+    return cap
 
-def readStream(cap: pyshark.capture.file_capture.FileCapture):
+def loadconfig(configfile):
+        with open(configfile, 'r') as stream:
+            try:
+                config = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        return config
+
+def readStream(cap: pyshark.capture.file_capture.FileCapture, rtp_list:dict={}, rtp_codec_list:dict={}):
     for frame in cap:
         try:
             rtp             = getRTPlayer(frame) 
             rtp_codec_list  = collectingCodecBySession(rtp, rtp_codec_list)
             rtp_list        = collectingPayloadBySession(rtp, rtp_list)
-        except:
-            pass
+        except Exception as e:
+            print(e)
 
-def audioSeparation(session):
+    print(f"Finish scrap: {pcap_file}")
+    return rtp_list, rtp_codec_list
+
+def audioSeparation(session, rtp_list, rtp_codec_list, outdir=''):
+    print(f"separation audio in session (ssrc): {session}")
     rtp_packet = rtp_list[session]
     codec = rtp_codec_list[session]
     fmt = usePyWavCodec(codec)
     payload = concatPayload(rtp_packet)
     audio = packetPayload2RawAudio(payload)
-    raw2wav(audio, fn=f'{session}.wav', fmt=fmt)
+    output = os.path.join(outdir, f'{session}.wav')
+    print(f"converting audio in session (ssrc): {session} to {output}")
+    raw2wav(audio, fn=output, fmt=fmt)
 
 if __name__ == "__main__":
-    pcap_file = (os.path.join('../test_180s.pcap'))
-    filter_type = "rtp.ssrc==0x5b6835b7"
+    pcap_file   = args['input'] if (args['input']) else (os.path.join('../test_180s.pcap'))
+    outdir      = args['outdir'] if (args['outdir']) else os.getcwd()
+    config      = loadconfig(args['config']) if (args['config']) else None
+    
+    # filter_type = "rtp.ssrc==0x5b6835b7"
+    filter_type = config['filter'] if config else 'rtp'
 
     cap = openPCAP(pcap_file, filter_type)
 
-    readStream(cap)
+    rtp_list, rtp_codec_list = readStream(cap)
 
     for rtp_ssrc in rtp_list:
-        audioSeparation(rtp_ssrc)
-
-    print("\nFinished outputing raw audio")
+        audioSeparation(rtp_ssrc, rtp_list, rtp_codec_list, outdir)
